7ac1dcea61f7af66bf2b171e7b3bb3e4d0399b81
zoheth <z0heth@outlook.com>
2024-01-16 23:34:16 +0800

    init commit

---
+++ xihe/xihe.cpp
@@ -0,0 +1,14 @@
+﻿// xihe.cpp : Defines the entry point for the application.
+//
+
+#include "xihe.h"
+#include <vulkan/vulkan.hpp>
+#include <volk.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
